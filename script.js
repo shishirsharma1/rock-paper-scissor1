2603328f2cc8adc17849d621a0a2d8a316d414ed
Shishir <123456789+odin@shishirsharma1104@gmail.com>
2024-01-16 20:00:02 +0530

    Add the play round and player selection functions

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 /*CONSTANTS*/
 const choices = ["ROCK", "PAPER", "SCISSOR"]
+const playerSelection = 'ROCK';
+let computerSelection = getComputerChoice();
 
+function getPlayerChoice(){
+    let playerChoice = prompt("What is your choice");
+    playerChoice = playerChoice.toUpperCase();
+    return playerChoice;
+}
 
 function getComputerChoice(){
     random = Math.floor(Math.random()*3);
@@ -8,3 +15,26 @@ function getComputerChoice(){
 
 }
 
+function playRound(computerSelection, playerSelection){
+    if(playerSelection == 'ROCK' && computerSelection =='PAPER'){
+        console.log("You Lose! Paper beats Rock");
+    }
+    else if(playerSelection == 'ROCK' && computerSelection =='SCISSOR'){
+        console.log("You Win! Rock beats Scissor");
+    }
+    else if(playerSelection == 'SCISSOR' && computerSelection =='PAPER'){
+        console.log("You Win! Scissor beats Paper");
+    }
+    else if(playerSelection == 'SCISSOR' && computerSelection =='ROCK'){
+        console.log("You lose! Rock beats Scissor");
+    }
+    else if(playerSelection == 'PAPER' && computerSelection =='ROCK'){
+        console.log("You Win! Paper beats Rock");
+    }
+    else if(playerSelection == 'PAPER' && computerSelection =='SCISSOR'){
+        console.log("You Lose! Scissor beats Paper");
+    }
+    else if(playerSelection == computerSelection){
+        console.log("Tie");
+    }
+}
